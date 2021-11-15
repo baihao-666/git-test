b56f8ba19310c4d5abdc0b3cf6f22a1784759a3a
Pride <3335344879@qq.com>
2021-11-15 21:06:16 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3   直接提交本地库");
+        System.out.println("hello git4 hot-fix");
     }
 }
